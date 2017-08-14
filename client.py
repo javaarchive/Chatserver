40506e64b8b775c1ab73e1dc4c5f84d310c96327
javaarchive <raymond.peng07@gmail.com>
2017-08-14 15:43:33 -0700

    Add files via upload

--- client.py
+++ client.py
@@ -1,10 +1,16 @@
+#Options
+server="fathhomepc1.t-mobile.com"
+port=8000
+backgroundcolor="#fff7e5"
+foregroundcolor="#73ff5e"
 import socket
 import os
+import time
 needkey=not os.path.exists("keyfile.key")
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 c=clientsocket
 
-clientsocket.connect(('localhost', 8000))
+clientsocket.connect((server, port))
 print(c.recv(8000))
 f="UTF-8"
 if needkey:
@@ -13,5 +19,44 @@ if needkey:
         key=c.recv(1000).decode("UTF-8")
         f.write(key)
         print(key)
+        name=input("You need a name: ")
+        c.send(bytes(name,"UTF-8"))
     f="UTF-8"
-        
+else:
+    with open("keyfile.key","r") as fi:
+
+        key=int(fi.readline())
+print("Loading connection gui")
+from tkinter import *
+from tkinter.ttk import *
+t=Tk()
+output=Text(t,bg=backgroundcolor,fg=foregroundcolor)
+output.pack()
+chatbox=Entry(t,width=120)
+def chat(enter=0):
+    global key
+    c.send(bytes("chatmessage","UTF-8"))
+    print(type(key))
+    c.send(str(key).encode("UTF-8"))
+    time.sleep(1)
+    c.send(bytes(chatbox.get(),"UTF-8"))
+    chatbox.delete("0",END)
+btn=Button(t,text="chat",command=chat)
+btn.pack()
+chatbox.pack()
+menu=Menu(t)
+chatcontrols=Menu(t)
+def updatescreen(x=0):
+    c.send(bytes("update","UTF-8"))
+    output.delete("0.0",END)
+    output.insert(END,c.recv(9000).decode("UTF-8"))
+def exit1(x=0):
+    c.send(bytes("exit","UTF-8"))
+def stop(x=0):
+    c.send(bytes("Shutdown","UTF-8"))
+chatcontrols.add_command(label="Update", command=updatescreen)
+chatcontrols.add_command(label="terminate server", command=stop)
+chatcontrols.add_command(label="Exit program", command=exit1)
+menu.add_cascade(label="Chat controls", menu=chatcontrols)
+t.config(menu=menu)
+t.mainloop()
--- server.py
+++ server.py
@@ -1,17 +1,36 @@
+import os
 import socket
 import threading
 import shelve as sh
+import signal
 sfile=sh.open("Ids.save")
 s=socket.socket()
-s.bind(("", 8000))
+s.bind((socket.gethostname(), 8000))#"" for local use, use socket.gethostname() for internat acess
 s.listen(100)
+stop=False
+with open("chat.save","r") as r:
+   chat=r.read()
+class ExitCommand(Exception):
+    pass
 
+
+def signal_handler(signal, frame):
+    raise ExitCommand()
+
+
+def thread_job():
+    global stop
+    while not stop:
+       pass
+    os.kill(os.getpid(), signal.SIGINT)
+signal.signal(signal.SIGINT, signal_handler)
+threading.Thread(target=thread_job).start()
 class serve (threading.Thread):
    def __init__(self, client):
       threading.Thread.__init__(self)
       self.c=client
    def run(self):
-        global stop
+        global stop,chat
         f="UTF-8"
         conn=self.c
         try:
@@ -21,18 +40,50 @@ class serve (threading.Thread):
                data = conn.recv(1024)
                if data.decode("UTF-8")=="Createkey":
                    newid=str(len(sfile)+1)
-                   sfile[newid]={"name":""}
+                   
                    conn.sendall(bytes(newid,f))
-               else:
-                   conn.sendall(bytes("still in progress",f))
+                   name=conn.recv(2094).decode()
+                   sfile[newid]={"name":name}
+                   chat=chat+"[Server]"+name+"joined the server      \n"
+               
+               #conn.sendall(bytes("still in progress",f))
+               if data.decode("UTF-8")=="Shutdown":
+                  stop=True
+               if data.decode("UTF-8")=="update":
+                  conn.send(bytes(chat,"UTF-8"))
+               if data.decode("UTF-8")=="chatmessage":
+                  sup=conn.recv(50)
+                  print(type(sup))
+                  print(sup)
+                  print(dir(sup))
+                  sup=sup.decode()
+                  print(type(sup))
+                  print(sup)
+                  sup=int(sup)
+                  dict1=sfile[str(sup)]
+                  
+       
+                  
+                  print("Incoming message"+str(dict1))
+                  chat=chat+"["+dict1["name"]+"]"+conn.recv(1092).decode("UTF-8")+"\n"
+                  with open("chat.save","w") as q:
+                      q.write(chat)
+               if data.decode("UTF-8")=="exit":
+                  
+                  break
+               
         finally:
             print("Connection exited")
 try:
-    while True:
+    while not stop:
         c,a=s.accept()
         x=serve(c)
         x.start()
 finally:
     print("Status program quit")
+    with open("chat.save","w") as q:
+       q.write(chat)
+       
+       
         
     
